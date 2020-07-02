8a052c4190801d35665d5ceaf828a135e272862c
jenslaufer <jenslaufer@gmail.com>
2020-07-02 14:24:51 +0200

    added ppc functions

---
+++ R/ppc.R
@@ -0,0 +1,34 @@
+cost_per_conversion <- function(cpc, cr) {
+  cpc / cr
+}
+
+num_of_conversions <- function(ad_budget, cpc, cr) {
+  ad_budget / cost_per_conversion(cpc, cr)
+}
+
+revenue <- function(ad_budget, product_cost, cpc, cr) {
+  num_of_conversions(ad_budget, cpc, cr) * product_cost
+}
+
+profit <- function(ad_budget, product_cost, cpc, cr) {
+  revenue(ad_budget, product_cost, cpc, cr) - ad_budget
+}
+
+ad_budget_for_profit <- function(profit, product_cost, cpc, cr) {
+  profit * cost_per_conversion(cpc, cr) / (product_cost - cost_per_conversion(cpc, cr))
+}
+
+traffic_for_profit <- function(profit, product_cost, cpc, cr, ctr) {
+  ad_budget_for_profit(profit, product_cost, cpc, cr) / (ctr * cpc)
+}
+
+profit_for_specified_traffic  <-
+  function(traffic, cpc, product_cost, cr, ctr) {
+    traffic_to_site <- traffic * ctr
+    ad_cost <- traffic_to_site * cpc
+    profit(ad_cost, product_cost, cpc, cr)
+  }
+
+product_cost_for_profit <- function(profit, ad_budget, cpc, cr) {
+  (profit + ad_budget) / num_of_conversions(ad_budget, cpc, cr)
+}
